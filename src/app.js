36e1463eb58b1290724c56913db111abd118cdad
soum3n0 <soumen0dey22@gmail.com>
2024-10-21 11:38:15 +0530

    init database

--- src/app.js
+++ src/app.js
@@ -1,10 +1,32 @@
 const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
-app.use("/", (req, res)=>{
-    res.send("Hello world");
-})
+app.post("/signup", async (req, res) => {
+    const userObj = {
+        firstName : "Kousik",
+        lastName : "Dey",
+        email : "kousik@gmail.com",
+        password : "Kousik@123",
+        gender : 'M'
+    };
 
-app.listen(3000, ()=>{
-    console.log("Connected to server");
+    const user = new User(userObj);
+
+    try{
+        await user.save();
+        res.send("User added successfully");
+    }catch(err){
+        res.status(400).send("Error saving the user : " + err.message);
+    }
+});
+
+connectDB().then(()=>{
+    console.log("Databae connected");
+    app.listen(3000, ()=>{
+        console.log("Connected to server");
+    })
+}).catch((err)=>{
+    console.error("Database is not connected");
 })
