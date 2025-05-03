245235cddc308e5603b8a409ecaedbe834fbaeec
soum3n0 <soumen0dey22@gmail.com>
2025-05-03 17:15:51 +0530

    render.com issue resolved

--- devTinder-UI/vite.config.js
+++ devTinder-UI/vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://devtinder-backend-5xo0.onrender.com', // backend server
-        changeOrigin: true,
-        // secure: false,
-      },
-    },
-  },
-})
+    plugins: [react()],
+});
