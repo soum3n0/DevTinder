650e49422edcd8effca682130fdee11a263e2753
soum3n0 <soumen0dey22@gmail.com>
2025-05-05 12:14:22 +0530

    chat#3 and route

--- devTinder-Backend/src/app.js
+++ devTinder-Backend/src/app.js
@@ -29,9 +29,9 @@ app.use("/", requestRouter);
 app.use("/", userRouter);
 app.use("/", chatRouter);
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 /*
